be79338bc198d6ed7c86f853099ce4327424dcee
penzija <luka.kik@iths.se>
2021-12-23 14:51:30 +0100

    Initial commit

---
+++ src/main/java/GameOfLife.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class GameOfLife {
+    public static ArrayList<Cell> createGrid(int xSize, int ySize) {
+        return null;
+    }
+}
---
+++ src/test/java/GameOfLifeTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GameOfLifeTest {
+
+    @Test
+    void test() {
+        int expectedResult = 9;
+        var actualResult = GameOfLife.createGrid(3, 3).size();
+        assertEquals(expectedResult, expectedResult);
+    }
+}
