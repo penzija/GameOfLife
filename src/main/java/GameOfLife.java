99d12afcda59e3746aacdef6e0921d21f73f8450
penzija <luka.kik@iths.se>
2022-01-07 16:40:28 +0100

    Testing screen display of grid (failed test)

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -10,12 +10,13 @@ public class GameOfLife {
         System.out.println("How big is the grid? (y) ");
         int gridXValue = 3;
         int gridYValue = 3;
+        int gridMaxX = 3;
 
         for (int i = 0; i < gridXValue * gridYValue; i++) {
             System.out.println("Is the cell dead or alive? ");
             deadOrAlive.add(true);
         }
-        return new GridParameter(gridXValue, gridYValue, deadOrAlive);  // HERE WE CREATED THE PARAMETERS FOR THE NEXT METHOD
+        return new GridParameter(gridXValue, gridYValue, deadOrAlive, gridMaxX);  // HERE WE CREATED THE PARAMETERS FOR THE NEXT METHOD
     }
 
     public static List<Cell> buildGrid(GridParameter gridParameters) {
@@ -32,5 +33,22 @@ public class GameOfLife {
             }
         }
         return cellsInGrid;
+    }
+
+    public static String stringOutput(ArrayList<Cell> cellsGrid) {
+
+        int maxX = 3;
+        String grid = "";
+
+        for (Cell cell : cellsGrid) {
+            if (cell.isAlive && cell.getX() == maxX) {
+                grid += ("X\r\n");
+            } else if (cell.isAlive) {
+                grid = grid + ("X");
+            } else {
+                grid += ("o");
+            }
+        }
+        return grid;
     }
 }
